45cd5ed5ff9e1ba8e9fa84ca9bd9ed3e3253242a
DmitryBelotsky <belotsky.dmitry@gmail.com>
2022-08-01 16:41:23 +0300

    init: create task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  return matrix && matrix.length ? matrix.map( (el, key) => key % 2 !== 0 ? el.reverse() : el).flat() : [];
 }
